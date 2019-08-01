53d89961bd6187d42a0eb6814ff70eaf7b55a626
Marcel Thomas <marcel@unubo.com>
2019-08-01 18:51:47 +0100

    Added handler function

--- server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-const PORT = process.env.PORT || 5555;
- 
-app.get('*', (req, res) => {
-  res.send('Node.js serverless deployed on Unubo Cloud.');
-});
-
-app.listen(PORT, () => console.log(`> Ready on http://localhost:${PORT}`));
