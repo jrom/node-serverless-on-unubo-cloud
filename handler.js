7b97816ca55947fe2c41fb078f3472c9b41f5392
Jordi Romero <jordi@jrom.net>
2020-02-21 12:29:19 +0100

    Automatic deploy?

--- handler.js
+++ handler.js
@@ -1,3 +1,5 @@
+const PACO = process.env.PACO;
+
 module.exports = (event, response) => {
-  response.status(200).succeed('Node.js serverless deployed on Unubo.');
+  response.status(200).succeed(`Node.js serverless deployed on Unubo. PACO = ${PACO}`);
 };
